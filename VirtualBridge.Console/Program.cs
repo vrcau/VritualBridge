1b23edcb04d0540c044dd58a3bfde9b0ded08aad
Misaka-L <lipww1234@foxmail.com>
2023-07-01 16:07:48 +0800

    feature: appsettings

--- VirtualBridge.Console/Program.cs
+++ VirtualBridge.Console/Program.cs
@@ -1,24 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Templates;
 using Serilog.Templates.Themes;
 using VirtualBridge.Core;
-using VirtualBridge.Core.Services;
 
 var logTemplate =
     "[{@t:yyyy-MM-dd HH:mm:ss} {@l:u3}] [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}] {@m}\n{@x}";
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.File(new ExpressionTemplate(logTemplate), "logs/app-.log", rollingInterval: RollingInterval.Day)
-    .WriteTo.Console(new ExpressionTemplate(logTemplate, theme: TemplateTheme.Literate))
-    .WriteTo.Debug(new ExpressionTemplate(logTemplate))
-    .CreateLogger();
-
-var host = new HostBuilder()
+var builder = Host.CreateDefaultBuilder(args)
+#if DEBUG
+    .UseEnvironment("Development")
+#endif
     .AddVirtualBridge()
     .UseSerilog()
-    .Build();
-    
-await host.RunAsync();
+    .ConfigureServices((context, _) =>
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(context.Configuration)
+            .WriteTo.File(new ExpressionTemplate(logTemplate), "logs/app-.log", rollingInterval: RollingInterval.Day)
+            .WriteTo.Console(new ExpressionTemplate(logTemplate, theme: TemplateTheme.Literate))
+            .WriteTo.Debug(new ExpressionTemplate(logTemplate))
+            .CreateLogger();
+    });
+
+await builder.Build().RunAsync();
